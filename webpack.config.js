2ae249ad50a6a0ff2c37f6f0e666c743a331a20e
Gustavo Ortiz <gortiz@paybygroup.com>
2020-02-19 16:03:54 -0600

    Fix annoying require bug

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,8 @@ const path = require('path');
    devServer: {
      contentBase: './dist',
    },
+   target: 'web',
    plugins: [
-     // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
      new CleanWebpackPlugin(),
      new HtmlWebpackPlugin({
        title: 'Crispy Enigma',
