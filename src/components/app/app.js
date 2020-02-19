a02595fa7ec50e3699bb42818a32d0a942ed3f51
Gustavo Ortiz <gortiz@paybygroup.com>
2020-02-19 12:37:00 -0600

    Setup an initial page with styles

---
+++ src/components/app/app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './style.scss';
+const App = () => (
+  <React.Fragment>
+    <header>
+      <div className="container">
+        <h1>Crispy Enigma</h1>
+      </div>
+    </header>
+    <div className="body">
+      <div className="container">
+      </div>
+    </div>
+    <footer>
+    </footer>
+  </React.Fragment>
+);
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
  const HtmlWebpackPlugin = require('html-webpack-plugin');
  const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
  module.exports = {
    mode: 'development',
@@ -16,6 +17,9 @@ const path = require('path');
      new HtmlWebpackPlugin({
        title: 'Crispy Enigma',
      }),
+     new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
    ],
    output: {
      filename: '[name].bundle.js',
@@ -30,6 +34,10 @@ const path = require('path');
         options: {
           presets: ['@babel/preset-env', '@babel/react'],
         },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
     ],
   },
